ca84e340b1937afd442a0074cd87fd9a39f8fec9
JerrystylMG <151972907+JerrystylMG@users.noreply.github.com>
2023-12-05 19:02:58 -0500

    Create Home.js

---
+++ components/Home.js
@@ -0,0 +1,13 @@
+// src/components/Home.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Native Copy!</h1>
+      <p>This is the main page content for your site.</p>
+    </div>
+  );
+};
+
+export default Home;
