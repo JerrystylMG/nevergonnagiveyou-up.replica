ddf8cb2c75be21ef31fd163a2b5ba130eac87e58
JerrystylMG <151972907+JerrystylMG@users.noreply.github.com>
2023-12-05 19:06:04 -0500

    Update Home.js

--- components/Home.js
+++ components/Home.js
@@ -3,10 +3,10 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to Native Copy!</h1>
-      <p>This is the main page content for your site.</p>
-    </div>
+  <h1>Hello! This is a replica of Never gonna give you up</h1>
+  <h3>Sorry if it doesn't look like the original, I'm going to try something like it.</h1>
+  <button onclick="window.document.href = '/index.html';">Math</button>
+
   );
 };
 
