00577383957122ac37381300aa69a6a9200eb7f4
JerrystylMG <151972907+JerrystylMG@users.noreply.github.com>
2023-12-05 19:03:26 -0500

    Create Subdomain.js

---
+++ components/Subdomain.js
@@ -0,0 +1,13 @@
+// src/components/Subdomain.js
+import React from 'react';
+
+const Subdomain = () => {
+  return (
+    <div>
+      <h1>Subdomain Page</h1>
+      <p>This is the content specific to the subdomain page.</p>
+    </div>
+  );
+};
+
+export default Subdomain;
